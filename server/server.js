efc9725160342cb36ddb4c870960203ac2575fb0
Towarek <Lukasz.Towarek@verint.com>
2017-11-28 14:32:54 +0000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -29,6 +30,27 @@ app.get('/todos', (req, res)=>{
     });
 });
 
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+            return res.status(404).send();
+         }
+         console.log('passed validation');
+    Todo.findById(id).then((todo)=>{
+        if(todo){
+            console.log(todo);
+            res.send({todo});
+        }
+        else{
+            res.status(404).send({});
+        }
+    }, (err)=>{
+        res.status(400).send({});
+    });
+});
+
+
+//Server 
 const PORT_NUMBER = 3000;
 app.listen(PORT_NUMBER, ()=>{
     console.log(`Listening on port ${PORT_NUMBER}`);
